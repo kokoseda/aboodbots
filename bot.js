f2ab3930bda888c3a67184c78b5435f544b5c0da
kokoseda <kokoseda22@yahoo.com>
2018-11-20 11:46:07 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const client = Discord.Client();
+
+client.on('ready', async() => {
+var server = "486308710428311583"; // ايدي السررفر
+var channel = "486335542925983744";//ايدي الروم
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('#daily')
+    },8.64e+7);
+})
+
+client.on('message', message => {
+if(message.content.startsWith('c1')) {
+if(message.author.id !== "314135031029170197") return;
+var args = message.content.split(' ').slice(1).join(' ');
+message.channel.send(args);
+}
+});
+
+client.login(process.env.BOT_TOKEN)
