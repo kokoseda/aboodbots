a4e97180f60561c3dd45ae7d6e61cb5d33d24da4
kokoseda <kokoseda22@yahoo.com>
2018-11-20 15:10:35 +0200

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 
 client.on('guildMemberAdd', member=> {
-    member.addRole(member.guild.roles.find("name","Member"));
+    member.addRole(member.guild.roles.find("name","member"));
     });
 
 
